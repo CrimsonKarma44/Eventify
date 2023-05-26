44a6150bbdcecc50c63561fe075bc78f510ce108
Okereke Princewill Vincent <vincentprincewill44@gmail.com>
2023-05-26 21:43:44 +0100

    model

--- Porfolio/Eventify/models.py
+++ Porfolio/Eventify/models.py
@@ -1,22 +1,63 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    id = models.CharField(max_length=60)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+
+    start = models.DateTimeField(auto_now=True)
+    end = models.DateTimeField(auto_now_add=True)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # def __str__(self):
+    #     return str(self.name)
 
 class Categories(models.Model):
     id = models.CharField(max_length=60)
+    # event_id = models.ForeignKey(Events)
+    type = models.CharField(max_length=200)
 
-class Review(models.Model)):
-    id = models.CharField(max_length=60)
-    pass
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # def __str__(self):
+    #     return str(self.name)
 
-class Events(models.Model):
+class Review(models.Model):
     id = models.CharField(max_length=60)
-    pass
+    # event_id = models.CharField(max_length=60)
+    email = models.CharField(max_length=200)
+    content = models.TextField()
+    rate = models.CharField(max_length=200)
 
-class Payments(models.Model):
-    id = models.CharField(max_length=60)
-    pass
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # def __str__(self):
+    #     return str(self.name)
 
 class Tickets(models.Model):
     id = models.CharField(max_length=60)
-    pass
+    # event_id = models.CharField(max_length=60)
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # def __str__(self):
+    #     return str(self.name)
+
+class Payments(models.Model):
+    id = models.CharField(max_length=60)
+    ticket_id = models.CharField(max_length=60)
+    price = models.FloatField()
+    email = models.CharField(max_length=100)
+    code = models.CharField(max_length=60)
+
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # def __str__(self):
+    #     return str(self.name)
