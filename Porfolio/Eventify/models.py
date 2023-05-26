74e5ae095017639572c1963f4534627f8713a6a0
Okereke Princewill Vincent <vincentprincewill44@gmail.com>
2023-05-26 22:13:49 +0100

    model

--- Porfolio/Eventify/models.py
+++ Porfolio/Eventify/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+
 class Events(models.Model):
-    id = models.CharField(max_length=60)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     type = models.CharField(max_length=200)
@@ -17,8 +17,7 @@ class Events(models.Model):
     #     return str(self.name)
 
 class Categories(models.Model):
-    id = models.CharField(max_length=60)
-    # event_id = models.ForeignKey(Events)
+    event_id = models.ForeignKey(Events, on_delete=models.CASCADE)
     type = models.CharField(max_length=200)
 
     updated = models.DateTimeField(auto_now=True)
@@ -27,8 +26,7 @@ class Categories(models.Model):
     #     return str(self.name)
 
 class Review(models.Model):
-    id = models.CharField(max_length=60)
-    # event_id = models.CharField(max_length=60)
+    event_id = models.ForeignKey(Events, on_delete=models.CASCADE)
     email = models.CharField(max_length=200)
     content = models.TextField()
     rate = models.CharField(max_length=200)
@@ -39,10 +37,10 @@ class Review(models.Model):
     #     return str(self.name)
 
 class Tickets(models.Model):
-    id = models.CharField(max_length=60)
-    # event_id = models.CharField(max_length=60)
+    event_id = models.ForeignKey(Events, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     price = models.FloatField()
+    avail_status = models.BooleanField()
 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -50,8 +48,7 @@ class Tickets(models.Model):
     #     return str(self.name)
 
 class Payments(models.Model):
-    id = models.CharField(max_length=60)
-    ticket_id = models.CharField(max_length=60)
+    ticket_id = models.ForeignKey(Tickets, on_delete=models.CASCADE)
     price = models.FloatField()
     email = models.CharField(max_length=100)
     code = models.CharField(max_length=60)
