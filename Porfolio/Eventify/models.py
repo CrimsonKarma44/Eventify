b50e985d8d5585f60aae164592b822ef31296a7c
Okereke Princewill Vincent <vincentprincewill44@gmail.com>
2023-05-26 21:35:36 +0100

    update

--- Porfolio/Eventify/models.py
+++ Porfolio/Eventify/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    id = models.CharField(max_length=60)
+
+class Review(models.Model)):
+    id = models.CharField(max_length=60)
+    pass
+
+class Events(models.Model):
+    id = models.CharField(max_length=60)
+    pass
+
+class Payments(models.Model):
+    id = models.CharField(max_length=60)
+    pass
+
+class Tickets(models.Model):
+    id = models.CharField(max_length=60)
+    pass
