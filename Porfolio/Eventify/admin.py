17696af5722440db4554dbb37dcb3fe244d66a92
Okereke Princewill Vincent <vincentprincewill44@gmail.com>
2023-05-27 11:24:27 +0100

    database

--- Porfolio/Eventify/admin.py
+++ Porfolio/Eventify/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review, Tickets, Payments, Categories, Events
 
 # Register your models here.
+admin.register(Review)
+admin.register(Tickets)
+admin.register(Payments)
+admin.register(Categories)
+admin.register(Events)
--- Porfolio/Porfolio/settings.py
+++ Porfolio/Porfolio/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'Eventify.apps.Baseconfig'
+    'Eventify.apps.EventifyConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'Porfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Eventify',
+        'USER': 'root',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
