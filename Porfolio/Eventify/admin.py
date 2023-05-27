5dba48532803336022db6ac5f29aea8436f23656
Okereke Princewill Vincent <vincentprincewill44@gmail.com>
2023-05-27 12:43:20 +0100

    update

--- Porfolio/Eventify/admin.py
+++ Porfolio/Eventify/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Review, Tickets, Payments, Categories, Events
 
 # Register your models here.
-admin.register(Review)
-admin.register(Tickets)
-admin.register(Payments)
-admin.register(Categories)
-admin.register(Events)
+admin.site.register(Review)
+admin.site.register(Tickets)
+admin.site.register(Payments)
+admin.site.register(Categories)
+admin.site.register(Events)
--- Porfolio/evet.sql
+++
@@ -1,2 +0,0 @@
-create database if exists Eventify;
-
