8be33ac545a34eb43ea1fbcd527eacaf2fe3ac63
Ousmane Konate <konatealim@gmail.com>
2023-06-22 15:33:59 +0200

    update README

--- Porfolio/Porfolio/settings.py
+++ Porfolio/Porfolio/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'eventify',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'pass',
         'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
